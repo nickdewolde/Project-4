740a15d1c3b47c8b4e310141eb7f10c48a0d6362
Nick de Wolde <nickdewolde@hotmail.com>
2018-02-05 09:13:00 +0100

    Niveau 2 toegevoegd, responsive en rekentool aangepast

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ function Red() {
   document.getElementById("Bestellen2").style.background = "#ff4444";
   document.getElementById("Bestellen3").style.background = "#ff4444";
   document.getElementById("Navigatie").className = "navigatie_blue";
+  document.getElementById("mobile_nav").className = "nav_menu_blue";
 }
 
 function Normaal() {
@@ -35,4 +36,74 @@ function Normaal() {
   document.getElementById("Bestellen2").style.background = "";
   document.getElementById("Bestellen3").style.background = "";
   document.getElementById("Navigatie").className = "navigatie";
+  document.getElementById("mobile_nav").className = "nav_menu";
+}
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 3000); // Change image every 2 seconds
+}
+
+function rekenToolBrons() {
+  var Brons = 5.95;
+  alert(
+    "Per maand: €"+Brons+"\n"+
+    "Per jaar: €"+Brons*12+"\n"+
+    "Per 5 jaar: €"+Brons*60
+  );
+}
+
+function rekenToolSilver() {
+  var Silver = 7.95;
+  alert(
+    "Per maand: €"+Silver+"\n"+
+    "Per jaar: €"+Silver*12+"\n"+
+    "Per 5 jaar: €"+Silver*60
+  );
+}
+
+function rekenToolGoud() {
+  var Goud = 10.96;
+  alert(
+    "Per maand: €"+Goud+"\n"+
+    "Per jaar: €"+Goud*12+"\n"+
+    "Per 5 jaar: €"+Goud*60
+  );
+}
+
+function rekenToolGeel() {
+  var Geel = 4;
+  alert(
+    "Per maand: €"+Geel+"\n"+
+    "Per jaar: €"+Geel*12+"\n"+
+    "Per 5 jaar: €"+Geel*60
+  );
+}
+
+function rekenToolOranje() {
+  var Oranje = 6;
+  alert(
+    "Per maand: €"+Oranje+"\n"+
+    "Per jaar: €"+Oranje*12+"\n"+
+    "Per 5 jaar: €"+Oranje*60
+  );
+}
+
+function rekenToolGroen() {
+  var Groen = 10;
+  alert(
+    "Per maand: €"+Groen+"\n"+
+    "Per jaar: €"+Groen*12+"\n"+
+    "Per 5 jaar: €"+Groen*60
+  );
 }
